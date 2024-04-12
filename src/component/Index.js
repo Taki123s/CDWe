fd274e992b99b2b4ce7bd3b1316263ea31fc5c5f
Raikaki <hcdat862002@gmail.com>
2024-04-12 14:50:45 +0700

    update index and topview

--- src/component/Index.js
+++ src/component/Index.js
@@ -3,16 +3,20 @@ import axios from 'axios'; // You may need to install axios for HTTP requests
 import './bootstrap.min.css'; // Import Bootstrap CSS
 import './owl.carousel.min.css';
 import Carousel from './Carousel';
-
-import '/Users/Admin/animeweb/src/css/ds/style.css'
+import Topview from './Topview'
+import '/CDweb2/src/css/ds/style.css'
 function AnimePage() {
     const [movies, setMovies] = useState([]);
     const [filter, setFilter] = useState('isAtoZ');
 
+    const [products, setProducts] = useState([]);
+
     useEffect(() => {
-        // Fetch movies based on filter
-        fetchMovies(filter);
-    }, [filter]);
+      fetch('YOUR_API_ENDPOINT_HERE')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+        .catch(error => console.error('Error:', error));
+    }, []); // Chạy chỉ một lần khi component được render
 
     const fetchMovies = async (filter) => {
         try {
@@ -50,51 +54,28 @@ function AnimePage() {
                 </div>
               </div>
               <div className="row">
-                <div className="col-lg-4 col-md-6 col-sm-6">
-                  <div className="product__item">
-                    <a href='#'>
-                    <div
-                      className="product__item__pic set-bg"
-                      style={{ backgroundImage: `url("https://i.pinimg.com/564x/d6/a9/e4/d6a9e4d944352aae84ccbac8f8d655fa.jpg")` }}
-                    >
-                      <div className="ep">18 / 18</div>
-                      <div className="view">
-                        <i className="fa fa-eye"></i> 9141
-                      </div>
-                    </div>
-                    <div className="product__item__text">
-                      <h5>
-                        <a href="#">The Seven Deadly Sins: Wrath of the Gods</a>
-                      </h5>
-                    </div></a>
-                  </div>
-                </div>
-                <div className="col-lg-4 col-md-6 col-sm-6">
-                  <div className="product__item">
-                    <div
-                      className="product__item__pic set-bg"
-                      style={{ backgroundImage: `url("img/trending/trend-2.jpg")` }}
-                    >
-                      <div className="ep">18 / 18</div>
-        
-                      <div className="view">
-                        <i className="fa fa-eye"></i> 9141
-                      </div>
-                    </div>
-                    <div className="product__item__text">
-                      <h5>
-                        <a href="#">Gintama Movie 2: Kanketsu-hen - Yorozuya yo Eien</a>
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      {products.map(movie => (
+        <div className="col-lg-4 col-md-6 col-sm-6" key={movie.id}>
+          <div className="product__item">
+            <div className="product__item__pic set-bg" style={{ backgroundImage: `url(${movie.image})` }}>
+              <div className="ep">{movie.episode} / {movie.totalEpisodes}</div>
+              <div className="view"><i className="fa fa-eye"></i> {movie.views}</div>
+            </div>
+            <div className="product__item__text">
+              <h5><a href="#">{movie.name}</a></h5>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
             </div>
           </div>
         </div>
       </div>
     </section>
+    <Topview/>
     </div>
+ 
         
     );
 }
---
+++ src/component/Topview.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import './bootstrap.min.css';
+
+const Topview = () => {
+  const { t } = useTranslation();
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    loadTopViewMoviesDay();
+  }, []);
+
+  const loadTopViewMovies = async (action) => {
+    try {
+      const response = await axios.get('http://localhost:8080/topView', {
+        params: { action: action }
+      });
+      setMovies(response.data);
+    } catch (error) {
+      console.error('Error fetching top view movies:', error);
+    }
+  };
+
+  return (
+    <div className="col-lg-4 col-md-6 col-sm-8">
+      <div className="product__sidebar">
+        <div className="product__sidebar__view">
+          <div className="section-title">
+            <h5>{t('content.topview')}</h5>
+          </div>
+          <ul className="filter__controls">
+            <li className="active" data-filter=".day" onClick={() => loadTopViewMovies('topDay')}>{t('day')}</li>
+            <li data-filter=".month" onClick={() => loadTopViewMovies('topMonth')}>{t('month')}</li>
+            <li data-filter=".years" onClick={() => loadTopViewMovies('topYear')}>{t('year')}</li>
+          </ul>
+          <br />
+          <div className="filter__gallery">
+            {movies.map(movie => (
+              <div key={movie.id} className="product__sidebar__view__item set-bg mix day"
+                style={{ backgroundImage: `url(${movie.avatars[0].name})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
+                <div className="ep">{movie.currentEpisode}/{movie.totalEpisode}</div>
+                <div className="rate">{movie.avgRate}<i className="fa fa-star" style={{ color: '#f3da35' }}></i></div>
+                <div className="view" style={{ bottom: '10px', right: '10px', top: 'unset' }}>
+                  <i className="fa fa-eye"></i> {movie.views}
+                </div>
+                <h5><a href={`/movieDetail?idMovie=${movie.id}`}>{movie.name}</a></h5>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topview
+;

3e4bbc7f68c4baefe45dc742c340c14609c561e5
Taki123s <20130305@st.hcmuaf.edu.vn>
2024-04-22 18:57:57 +0700

    Watching Page ( tạm thời)

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./bootstrap.min.css";
 import "./owl.carousel.min.css";
 import "../css/ds/style.css";
+
 import logo from '../img/logo.png'
 // let searchResultsVisible = false;
 // function toggleSearchResults() {
@@ -61,8 +62,8 @@ function HeaderPage() {
                         <nav className="header__menu mobile-menu" id="nav">
                             <ul style={{justifyContent:`flex-start`,textWrap:`nowrap`}}>
                                 <li><a href="">Trang chủ</a></li>
-                                <li><a href="#"><span
-                                        className="arrow_carrot-down">Thể loại</span></a>
+                                <li><a href="#"
+                                        className="arrow_carrot-down">Thể loại</a>
                                     <div className="dropdown">
                                         <ul className="genreDropdown">
                                             {/* Render ra the loai */}

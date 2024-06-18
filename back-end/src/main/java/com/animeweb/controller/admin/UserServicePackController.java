7289bd10fb60ac6d86b9d42f69bcbdbce272a5d6
huynhtham308 <huynhtham3082@gmail.com>
2024-06-18 09:11:54 +0700

    add authorize

--- back-end/src/main/java/com/animeweb/controller/admin/UserServicePackController.java
+++ back-end/src/main/java/com/animeweb/controller/admin/UserServicePackController.java
@@ -5,6 +5,7 @@ import com.paypal.orders.Money;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
-@RequestMapping("/admin/userservice/")
+@RequestMapping("/admin/userservice")
 @RestController
 @RequiredArgsConstructor
 
@@ -20,11 +21,13 @@ public class UserServicePackController {
     private final UserPackedService userPackedService;
 
     @GetMapping("/month/{month}")
+    @PreAuthorize("hasAuthority('view_revenue') or hasRole('ADMIN')")
     public ResponseEntity<Long> GetRevenueByMonth(@PathVariable Long month) {
 
         return ResponseEntity.ok(userPackedService.GetRevenueByMonth(month));
     }
     @GetMapping("/revenue")
+    @PreAuthorize("hasAuthority('view_revenue') or hasRole('ADMIN')")
     public ResponseEntity<Long> GetRevenueByMonth() {
         return ResponseEntity.ok(userPackedService.getRevenue());
     }
